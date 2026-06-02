49d0678a138874e77111b79099e9ac6be37b62e3
Ankit Walishetti <125695097+titan-ankit@users.noreply.github.com>
2026-06-02 16:33:02 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        known_nums = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in known_nums:
+                first_index = known_nums[target - nums[i]]
+                return [first_index, i]
+            else:
+                known_nums[nums[i]] = i # match key value pair in the dictionary 
+
